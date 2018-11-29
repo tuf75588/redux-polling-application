8d2796b97949327f522b9741c137be3065042e0f
Andrew Davis <tuf75588@temple.edu>
2018-11-29 17:10:34 -0500

    add reducers and create store for app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers/index';
+import './index.css';
+import App from './components/App';
+import middleware from './middleware';
+import { handleInitialData } from './actions/shared';
+const store = createStore(reducers, middleware);
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App dispatch={store.dispatch} />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/middleware/index.js
@@ -0,0 +1,5 @@
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from './logger';
+const middleware = applyMiddleware(thunk, logger);
+export default middleware;
