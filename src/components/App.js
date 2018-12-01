7ba5401e4bffb0a11d6de44c309ca0d59f04da2e
Andrew Davis <tuf75588@temple.edu>
2018-12-01 18:10:47 -0500

    loading reducer component added

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { handleInitialData } from '../actions/shared';
 import { connect } from 'react-redux';
 import Dashboard from './Dashboard';
+import LoadingBar from 'react-redux-loading';
 class App extends Component {
   componentDidMount() {
     //! call handleInitialData to get data from our store
@@ -9,7 +10,12 @@ class App extends Component {
     dispatch(handleInitialData());
   }
   render() {
-    return <div>{this.props.loading ? <h1>Loading...</h1> : <Dashboard />}</div>;
+    return (
+      <div>
+        <LoadingBar />
+        {this.props.loading ? null : <Dashboard />}
+      </div>
+    );
   }
 }
 function mapStateToProps({ authedUser }) {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authedUser from './authedUser';
+import { loadingBarReducer } from 'react-redux-loading';
 import users from './users';
 import polls from './polls';
 export default combineReducers({
+  loadingBar: loadingBarReducer,
   users,
   polls,
   authedUser
