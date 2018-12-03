85c512b478f5c1c378d60b17dd6e887691a45edf
Andrew Davis <tuf75588@temple.edu>
2018-12-02 21:46:30 -0500

    new thunk dispatch function

--- src/actions/polls.js
+++ src/actions/polls.js
@@ -1,8 +1,20 @@
+import { savePoll } from '../utils/api';
 export const RECEIVE_POLLS = 'RECEIVE_POLLS';
-
+export const ADD_POLL = 'ADD_POLL';
 export function receivePolls(polls) {
   return {
     type: RECEIVE_POLLS,
     polls
+  };
+}
+export function handleAddPoll(poll) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+  };
+}
+function addPoll(poll) {
+  return {
+    type: ADD_POLL,
+    poll
   };
 }
