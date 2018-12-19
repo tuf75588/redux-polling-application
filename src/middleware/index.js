f6a8d49347e2bfabeeeb6b182c73ef26263954d5
Andrew Davis <atd285@gmail.com>
2018-12-19 15:17:13 -0500

    add redux dev tools

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,6 @@
-import { applyMiddleware } from 'redux';
+import { applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from './logger';
-const middleware = applyMiddleware(thunk, logger);
-export default middleware;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default composeEnhancers(applyMiddleware(thunk, logger));
